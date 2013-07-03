5f02725d5e2715daa0931fbaf1c576c6260a1fd8
Giles Bowkett <gilesb@gmail.com>
2013-07-02 21:27:43 -0700

    get trigrams

--- src/markov_bass_lines/core.clj
+++ src/markov_bass_lines/core.clj
@@ -3,12 +3,17 @@
 (def transcribed-bass-lines (list '(on off on tie on off on tie)
                                   '(on tie off on tie off on on)))
 
-(defn tri-gram [bass-lines] (take 3 (first bass-lines)))
-
-(def bi-gram (take 2 (tri-gram transcribed-bass-lines)))
-(def next-note (nth (tri-gram transcribed-bass-lines) 2))
-(def bi-gram-next-note {bi-gram next-note})
+(defn trigrams [bassline]
+  (if (> 3 (count bassline))
+    ()
+    (cons (list (list (first bassline)
+                      (second bassline))
+                (nth bassline 2))
+          (trigrams (rest bassline)))))
 
 (defn -main []
-  (println (bi-gram-next-note '(on off))))
+  (println (first transcribed-bass-lines))
+  (println (trigrams (first transcribed-bass-lines)))
+  (println (second transcribed-bass-lines))
+  (println (trigrams (second transcribed-bass-lines))))
 
