c77b02c9009972abd18d46bc4e5c1d61a2ba6d13
Giles Bowkett <gilesb@gmail.com>
2013-07-02 20:56:44 -0700

    converted tri-gram to a function

--- src/markov_bass_lines/core.clj
+++ src/markov_bass_lines/core.clj
@@ -3,9 +3,10 @@
 (def transcribed-bass-lines (list '(on off on tie on off on tie)
                                   '(on tie off on tie off on on)))
 
-(def trigram (take 3 (first transcribed-bass-lines)))
-(def bi-gram (take 2 trigram))
-(def next-note (nth trigram 2))
+(defn tri-gram [bass-lines] (take 3 (first bass-lines)))
+
+(def bi-gram (take 2 (tri-gram transcribed-bass-lines)))
+(def next-note (nth (tri-gram transcribed-bass-lines) 2))
 (def bi-gram-next-note {bi-gram next-note})
 
 (defn -main []
