8a9ec357ac96ee2a1db44c4433e1e34e04f1b67d
Giles Bowkett <gilesb@gmail.com>
2013-07-02 20:53:20 -0700

    list of lists

--- src/markov_bass_lines/core.clj
+++ src/markov_bass_lines/core.clj
@@ -1,6 +1,9 @@
 (ns markov-bass-lines.core)
 
-(def trigram (take 3 (seq '(on off on tie on off on tie))))
+(def transcribed-bass-lines (list '(on off on tie on off on tie)
+                                  '(on tie off on tie off on on)))
+
+(def trigram (take 3 (first transcribed-bass-lines)))
 (def bi-gram (take 2 trigram))
 (def next-note (nth trigram 2))
 (def bi-gram-next-note {bi-gram next-note})
