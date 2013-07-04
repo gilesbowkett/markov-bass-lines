4885d34241311b9542f43e8606f3e937dadb5898
Giles Bowkett <gilesb@gmail.com>
2013-07-03 18:22:08 -0700

    get n-grams from bassline corpus, for any n

--- src/markov_bass_lines/core.clj
+++ src/markov_bass_lines/core.clj
@@ -1,18 +1,46 @@
 (ns markov-bass-lines.core)
 
-(def transcribed-bass-lines (list '(on off on tie on off on tie)
-                                  '(on tie off on tie off on on)))
+(def fg "you have to quit the repl with control-D first")
 
-; much shorter version from https://gist.github.com/gilesbowkett/5915632
-(defn freqs []
-  (apply merge-with + (map (comp frequencies (partial partition 3 1)) transcribed-bass-lines)))
+(def titles-and-basslines
+  '((NTH_110_A_FunkBass
+     (on tie off on on tie off off on tie on on off on off on on tie on on off on off on on on on on tie off on off))
 
-; ???
+    (NTH_110_G_NiceFunkBass
+     (on off off on off off on off off off off off off off off on on off on on off off on off off off off off off off off off))
 
-(defn -main []
-  (freqs))
+    (NTH_110_G_SlinkyBass
+     (on tie on off on tie off on off on tie on on on tie off on tie on off on tie off on on on on off on tie off off))
 
+    (NTH_120_G_DiscoFunkBass
+     (on tie on tie on tie on tie on tie on on tie off on off on tie on tie on tie on tie on tie on on tie off on off))
 
-; markov-bass-lines.core=> (frequencies '(("on off" "on") ("on tie" "off") ("on off" "on")))
-; {("on off" "on") 2, ("on tie" "off") 1}
+    (NTH_120_G_SpaceDiscoBass
+     (on tie off off off off off on on tie off off off off off on on on on tie off on tie off on tie off on on tie on tie ))
+
+    (NTH_125_G_SubFunkBass
+     (on tie off off on tie off off on tie off off on on off on tie on on off on tie on tie on tie on on tie on tie off))
+
+    (NTH_130_C#_ElectricDiscoBass
+     (on tie on off off on off on on tie off off off off off on on tie on tie off on off on on tie off off off off off off))
+
+    (NTH_130_C#_ElectricMenaceBass
+     (on tie off on off off on off on off on off on on off on on tie off on off off on off on off on off on off on off))
+
+    (NTH_130_C#_SmoothBass
+     (on tie tie tie off off on off on tie off off off off on tie on tie on tie off off on tie on tie on tie off off off off))
+
+    (NTH_130_G_SawFunkBass
+     (on tie off off on tie off off on tie on on off on on on on tie on tie on tie off on tie off off off off off off off))))
+
+(def basslines (map (fn [title-and-notes] (first (rest title-and-notes))) titles-and-basslines))
+
+(defn n-gram-freqs "get n-grams of arbitrary size, and their frequencies" [n]
+  (apply merge-with
+   +
+   (map (comp frequencies
+              (partial partition n 1))
+        basslines)))
+
+(defn -main [])
 
