19d71223736c1f381d977bbd9f14f3440c38d139
Giles Bowkett <gilesb@gmail.com>
2013-07-03 22:34:59 -0700

    basic markov!! (first-order, choosing one note)

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]])
--- src/markov_bass_lines/core.clj
+++ src/markov_bass_lines/core.clj
@@ -1,6 +1,5 @@
-(ns markov-bass-lines.core)
-
-(def fg "you have to quit the repl with control-D first")
+(ns markov-bass-lines.core
+  (:require [clojure.math.numeric-tower :as math]))
 
 (def titles-and-basslines
   '((NTH_110_A_FunkBass
@@ -37,12 +36,92 @@
                         (first (rest title-and-notes)))
                     titles-and-basslines))
 
-(defn n-gram-freqs "get n-grams of arbitrary size, and their frequencies" [n]
+(defn n-gram-freqs
+      "get n-grams of arbitrary size, and their frequencies"
+      [n]
   (apply merge-with
    +
    (map (comp frequencies
               (partial partition n 1))
         basslines)))
+
+; start on the downbeat. true for every one of the corpus basslines, and
+; probably every funk bassline since the dawn of time.
+(def first-note 'on)
+
+(defn translate-frequencies-into-probabilities []
+  ; the problem: get from here --> {(tie tie) 1, (tie on) 3}
+  ;                    to here --> {tie {tie 0.25, on 0.75}}
+
+  ;              and from here --> {(off on off) 22, (off on on) 16, (off on tie) 19}
+  ;                    to here --> {"off on" [{off 0.39, on 0.28, tie 0.33}
+
+  "oops")
+
+(defn determine-probabilities [tokens-and-frequencies]
+  (map (fn [t-and-f]
+           (let [freq (last t-and-f)
+                       tokens (first t-and-f)]
+             {tokens (/ freq
+                        (apply +
+                               (vals tokens-and-frequencies)))}))
+       tokens-and-frequencies))
+
+(defn relevant-n-grams-only [tokens-and-frequencies target-token]
+  (filter (fn [t-and-f]
+            (= target-token
+               (first (first t-and-f))))
+          tokens-and-frequencies))
+
+; markov-bass-lines.core=> (determine-probabilities (relevant-n-grams-only '{(tie tie) 1, (tie on) 3, (foo bar) 1} 'tie))
+; ({(tie tie) 1/4} {(tie on) 3/4})
+
+; the next thing is find the lowest common denominator of this map, multiply each of the tokens by that number, and
+; put them all in a big list. then just pull a random element from that list; voila, Markov. then you add more
+; sophistication later, maybe.
+
+(defn denominators [probability-map]
+  (distinct (map (fn [fraction] (denominator (first (vals fraction)))) probability-map)))
+
+(defn denominator-for-markov-list [probability-map]
+  (let [denoms (denominators probability-map)]
+    (if (= 1 (count denoms))
+      (first denoms)
+      (reduce math/lcm denoms))))
+
+(defn normalize-markov-elements [token-fraction-pair list-denom]
+  (let [possible-choice (second (first (first token-fraction-pair)))
+        num (last (last token-fraction-pair))]
+    (repeat (* list-denom num) possible-choice)))
+
+(defn make-markov-list [probability-map]
+  (flatten (map (fn [t-f-pair]
+                    (normalize-markov-elements t-f-pair
+                                               (denominator-for-markov-list probability-map)))
+                probability-map)))
+
+; now randomly select an element from the above list
+(defn second-note []
+  (rand-nth
+    (make-markov-list
+      (determine-probabilities
+        (relevant-n-grams-only (n-gram-freqs 2)
+                               first-note)))))
+
+; this does not work for shit
+  (let [freqs (n-gram-freqs 2)
+        choices '((on off) ; this list should be dynamically generated
+                  (on on)  ; e.g., (choices (freqs first-note))
+                  (on tie))]
+    (freqs (first choices))))
+
+; gotta get my dev on
+
+(def fg "you have to quit the repl with control-D first")
+
+(def example (determine-probabilities (relevant-n-grams-only (n-gram-freqs 2) 'on)))
+
+; lein bs
 
 (defn -main [])
 
